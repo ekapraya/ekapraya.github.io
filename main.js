b33698814b841992f47638abe5002145d31db568
iniberas <nibrasramzy18@gmail.com>
2023-09-11 22:15:00 +0700

    test

--- main.js
+++ main.js
@@ -8,11 +8,12 @@ class Timer {
             seconds: root.querySelector(".seconds"),
             control: root.querySelector(".pause-resume-button"),
             reset: root.querySelector(".reset-button"),
-            dispay: root.querySelector(".time-display"),
+            display: root.querySelector(".time-display"),
             input: root.querySelector(".time-input")
         };
 
         this.interval = null;
+        this.initialSeconds = 0;
         this.remainingSeconds = 0;
 
         this.el.control.addEventListener("click", () => {
@@ -24,24 +25,48 @@ class Timer {
         });
 
         this.el.reset.addEventListener("click", () => {
-            const inputMinutes = prompt("Enter number of minutes:");
-      
-            if (inputMinutes < 60) {
-                this.stop();
-                this.remainingSeconds = inputMinutes * 60;
-                this.updateInterfaceTime();
-            }
+            this.stop();
+            this.remainingSeconds = this.initialSeconds;
+            this.updateInterfaceTime();
         });
 
         this.el.display.addEventListener("click", () => {
-            input.focus();
+            this.el.input.style.display = "block";
+            this.el.input.focus();
+        });
+
+        this.el.input.addEventListener("input", () => {
+            if (this.el.input.value[0] == "0") {
+                this.el.input.value = this.el.input.value.slice(1,7)
+            } else {
+                this.el.input.value = this.el.input.value.slice(0,6)
+            }
+            var value = this.el.input.value.padStart(6,"0");
+            this.el.input.value = value;
+            this.el.seconds.textContent = value.slice(4,6);
+            this.el.minutes.textContent = value.slice(2, 4);
+            this.el.hours.textContent = value.slice(0,2);
+            console.log(value)
+        });
+
+        this.el.input.addEventListener("change", () => {
+            this.stop();
+            var value = this.el.input.value
+            this.remainingSeconds = Number(value.slice(0, 2) * 3600) + Number(value.slice(2, 4) * 60) + Number(value.slice(4,6));
+            this.initialSeconds = this.remainingSeconds;
+            console.log(this.remainingSeconds)
+            this.updateInterfaceTime();
+            this.el.input.value = ""
+            this.el.input.style.display = "none";
         });
     }
 
     updateInterfaceTime() {
-        const minutes = Math.floor(this.remainingSeconds / 60);
+        const hours = Math.floor(this.remainingSeconds / 3600);
+        const minutes = Math.floor((this.remainingSeconds - hours * 3600)/ 60);
         const seconds = this.remainingSeconds % 60;
     
+        this.el.hours.textContent = hours.toString().padStart(2, "0");
         this.el.minutes.textContent = minutes.toString().padStart(2, "0");
         this.el.seconds.textContent = seconds.toString().padStart(2, "0");
     }
@@ -49,12 +74,12 @@ class Timer {
     updateInterfaceControls() {
         if (this.interval === null) {
             this.el.control.innerHTML = `<span class="material-symbols-rounded">play_arrow</span>`;
-            this.el.control.classList.add("timer__btn--start");
-            this.el.control.classList.remove("timer__btn--stop");
+            this.el.control.classList.add("timer-start");
+            this.el.control.classList.remove("timer-stop");
         } else {
             this.el.control.innerHTML = `<span class="material-symbols-rounded">pause</span>`;
-            this.el.control.classList.add("timer__btn--stop");
-            this.el.control.classList.remove("timer__btn--start");
+            this.el.control.classList.add("timer-stop");
+            this.el.control.classList.remove("timer-start");
         }
     }
 
@@ -83,7 +108,7 @@ class Timer {
 
     static getHTML() {
         return `
-        <input type="number" name="time-input" id="time-input" style="display: none;" class="time-input">
+        <input type="number" name="time-input" id="time-input" style="opacity: 0; position: absolute;" class="time-input">
 		<button class="time-display">
 			<span class="hours">00</span>
 			<span>:</span>
